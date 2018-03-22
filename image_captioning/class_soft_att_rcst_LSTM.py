083f016956fef7697156b58c4751bee831e17960
laviechen <laviechen@tencent.com>
2018-03-22 13:44:58 +0800

    update

--- image_captioning/class_soft_att_rcst_LSTM.py
+++ image_captioning/class_soft_att_rcst_LSTM.py
@@ -12,24 +12,25 @@ import torch.nn.functional as F
 from torch.autograd import *
 
 from classLSTMSoftAttCore import LSTMSoftAttentionCore
+from classLSTMCore import LSTMCore
 
 
 class ShowAttendTellModel(nn.Module):
     def __init__(self, opt):
         super(ShowAttendTellModel, self).__init__()
-        
+
         self.vocab_size = opt.vocab_size
         self.input_encoding_size = opt.input_encoding_size
         self.lstm_size = opt.lstm_size
         self.drop_prob_lm = opt.drop_prob_lm
         self.seq_length = opt.seq_length
-
+        
         self.fc_feat_size = opt.fc_feat_size
         self.conv_feat_size = opt.conv_feat_size
         self.conv_att_size = opt.conv_att_size
         self.att_hidden_size = opt.att_hidden_size
         self.ss_prob = opt.ss_prob  # Schedule sampling probability
-        
+
         self.fc2h = nn.Linear(self.fc_feat_size, self.lstm_size)
 
         self.core = LSTMSoftAttentionCore(self.input_encoding_size,
@@ -42,6 +43,12 @@ class ShowAttendTellModel(nn.Module):
         self.embed = nn.Embedding(self.vocab_size + 1, self.input_encoding_size)
         self.logit = nn.Linear(self.lstm_size, self.vocab_size)
 
+        # add the following parameters for ARNet
+        self.rcst_time = opt.rcst_time
+        self.rcst_scale = opt.rcst_weight  # lambda in ARNet
+        self.rcstLSTM = LSTMCore(self.lstm_size, self.lstm_size, self.drop_prob_lm)  # ARNet is realized by LSTM network
+        self.h_2_pre_h = nn.Linear(self.lstm_size, self.lstm_size)  # fully connected layer in ARNet
+
         self.init_weights()
 
     def init_weights(self):
@@ -50,6 +57,21 @@ class ShowAttendTellModel(nn.Module):
         self.fc2h.weight.data.uniform_(-initrange, initrange)
         self.logit.weight.data.uniform_(-initrange, initrange)
         self.logit.bias.data.fill_(0)
+        
+        # initialize weights of parameters in ARNet
+        self.h_2_pre_h.weight.data.uniform_(-initrange, initrange)
+        self.h_2_pre_h.bias.data.fill_(0)
+
+    def copy_weights(self, model_path):
+        """
+        Initialize the weights of parameters from the model 
+        which is pre-trained by Cross Entropy (MLE)
+        """
+        src_weights = torch.load(model_path)
+        own_dict = self.state_dict()
+        for key, var in src_weights.items():
+            print("copy weights: {}  size: {}".format(key, var.size()))
+            own_dict[key].copy_(var)
 
     def forward(self, fc_feats, att_feats, seq):
         batch_size = fc_feats.size(0)
@@ -58,9 +80,9 @@ class ShowAttendTellModel(nn.Module):
         init_h = init_h.unsqueeze(0)
         init_c = init_h.clone()
         state = (init_h, init_c)
-        outputs = []
 
-        for i in range(seq.size(1)-1):
+        outputs = []
+        for i in range(seq.size(1)):
             if i >= 1 and self.ss_prob > 0.0:
                 sample_prob = fc_feats.data.new(batch_size).uniform_(0, 1)
                 sample_mask = sample_prob < self.ss_prob
@@ -76,7 +98,7 @@ class ShowAttendTellModel(nn.Module):
                 it = seq[:, i].clone()
 
             # break if all the sequences end
-            if seq[:, i].data.sum() == 0:
+            if i >= 1 and seq[:, i].data.sum() == 0:
                 break
 
             xt = self.embed(it)
@@ -85,12 +107,69 @@ class ShowAttendTellModel(nn.Module):
             output = F.log_softmax(self.logit(output.squeeze(0)), dim=1)
             outputs.append(output)
 
-        vocab_log_probs = torch.cat([_.unsqueeze(1) for _ in outputs], 1).contiguous()
+        return torch.cat([_.unsqueeze(1) for _ in outputs], 1).contiguous()  # batch * 19 * vocab_size
 
-        return vocab_log_probs  # e.g. batch * 19 * vocab_size
+    # reconstruct 部分
+    def rcst_forward(self, fc_feats, att_feats, seq, mask):
+        batch_size = fc_feats.size(0)
+
+        init_h = self.fc2h(fc_feats)
+        init_h = init_h.unsqueeze(0)
+        init_c = init_h.clone()
+        state = (init_h, init_c)
+
+        rcst_init_h = init_h.clone()
+        rcst_init_c = init_c.clone()
+        rcst_state = (rcst_init_h, rcst_init_c)
+
+        pre_h = []
+        output_logits = []
+        rcst_loss = 0.0
+
+        for i in range(seq.size(1) - 1):
+            if i >= 1 and self.ss_prob > 0.0:  # otherwise no need to sample
+                sample_prob = fc_feats.data.new(batch_size).uniform_(0, 1)
+                sample_mask = sample_prob < self.ss_prob
+                if sample_mask.sum() == 0:
+                    it = seq[:, i].clone()
+                else:
+                    sample_ind = sample_mask.nonzero().view(-1)
+                    it = seq[:, i].data.clone()
+                    prob_prev = torch.exp(output_logits[-1].data)  # fetch prev distribution: shape Nx(M+1)
+                    it.index_copy_(0, sample_ind, torch.multinomial(prob_prev, 1).view(-1).index_select(0, sample_ind))
+                    it = Variable(it, requires_grad=False)
+            else:
+                it = seq[:, i].clone()
+
+            # break if all the sequences end
+            if i >= 1 and seq[:, i].data.sum() == 0:
+                break
+
+            xt = self.embed(it)
+            output, state = self.core.forward(xt, att_feats, state)
+            logit_words = F.log_softmax(self.logit(output.squeeze(0)))
+            output_logits.append(logit_words)
+
+            if i >= 1:
+                rcst_output, rcst_state = self.rcstLSTM.forward(output, rcst_state)
+                rcst_h = F.leaky_relu(self.h_2_pre_h(rcst_output))
+                rcst_t = pre_h[i - 1].squeeze(dim=0)
+
+                # -1 means not changing the size of that dimension,
+                # http://pytorch.org/docs/master/tensors.html
+                rcst_mask = mask[:, i].contiguous().view(batch_size, -1).expand(batch_size, self.lstm_size)
+                rcst_diff = rcst_h - rcst_t
+                rcst_loss += torch.sum(torch.sum(torch.mul(rcst_diff, rcst_diff) * rcst_mask, dim=1)) / batch_size * self.rcst_scale
+
+            # 更新 previous hidden state
+            pre_h.append(state[0].clone())
+
+        output_logits = torch.cat([_.unsqueeze(1) for _ in output_logits], 1).contiguous()
+
+        return output_logits, rcst_loss
 
     def sample_beam(self, fc_feats, att_feats, init_index, opt={}):
-        beam_size = opt.get('beam_size', 3)
+        beam_size = opt.get('beam_size', 10)  # 如果不能取到 beam_size 这个变量, 则令 beam_size 为 10
         batch_size = fc_feats.size(0)
         fc_feat_size = fc_feats.size(1)
 
@@ -114,26 +193,24 @@ class ShowAttendTellModel(nn.Module):
             beam_seq = torch.LongTensor(self.seq_length, beam_size).zero_()
             beam_seq_logprobs = torch.FloatTensor(self.seq_length, beam_size).zero_()
             beam_logprobs_sum = torch.zeros(beam_size)  # running sum of logprobs for each beam
-
             for t in range(self.seq_length + 1):
-                if t == 0:
+                if t == 0:  # input <bos>
                     it = fc_feats.data.new(beam_size).long().fill_(init_index)
                     xt = self.embed(Variable(it, requires_grad=False))
+                    # xt = self.img_embed(fc_feats[k:k+1]).expand(beam_size, self.input_encoding_size)
                 else:
-                    # lets go to CPU for more efficiency in indexing operations
-                    logprobsf = logprobs.float()
-
-                    # sorted array of logprobs along each previous beam (last true = descending)
+                    """perform a beam merge. that is,
+                    for every previous beam we now many new possibilities to branch out
+                    we need to resort our beams to maintain the loop invariant of keeping
+                    the top beam_size most likely sequences."""
+                    logprobsf = logprobs.float()  # lets go to CPU for more efficiency in indexing operations
                     # ys: beam_size * (Vab_size + 1)
-                    ys, ix = torch.sort(logprobsf, 1, True)
-
+                    ys, ix = torch.sort(logprobsf, 1, True)  # sorted array of logprobs along each previous beam (last true = descending)
                     candidates = []
-
                     cols = min(beam_size, ys.size(1))
                     rows = beam_size
                     if t == 1:  # at first time step only the first beam is active
                         rows = 1
-
                     for c in range(cols):
                         for q in range(rows):
                             # compute logprob of expanding beam q with word in (sorted) position c
@@ -141,8 +218,7 @@ class ShowAttendTellModel(nn.Module):
                             candidate_logprob = beam_logprobs_sum[q] + local_logprob
                             if t > 1 and beam_seq[t - 2, q] == 0:
                                 continue
-                            candidates.append({'c': ix.data[q, c],
-                                               'q': q,
+                            candidates.append({'c': ix.data[q, c], 'q': q,
                                                'p': candidate_logprob.data[0],
                                                'r': local_logprob.data[0]})
 
@@ -183,11 +259,13 @@ class ShowAttendTellModel(nn.Module):
 
                     # encode as vectors
                     it = beam_seq[t - 1]
-                    xt = self.embed(Variable(it.cuda(), requires_grad=False))
+                    xt = self.embed(Variable(it.cuda()))
+
+                if t >= 1:
                     state = new_state
 
                 output, state = self.core.forward(xt, att_feats_current, state)
-                logprobs = F.log_softmax(self.logit(output), dim=1)
+                logprobs = F.log_softmax(self.logit(output))
 
             self.done_beams[k] = sorted(self.done_beams[k], key=lambda x: -x['p'])
             seq[:, k] = self.done_beams[k][0]['seq']  # the first beam has highest cumulative score
@@ -202,7 +280,6 @@ class ShowAttendTellModel(nn.Module):
                 top_prob[k].append(self.done_beams[k][temp_index]['p'])
 
             top_seq.append(top_seq_cur)
-
         # return the samples and their log likelihoods
         return seq.transpose(0, 1), seqLogprobs.transpose(0, 1), top_seq, top_prob
 
@@ -225,7 +302,7 @@ class ShowAttendTellModel(nn.Module):
         state = (init_h, init_c)
 
         for t in range(self.seq_length):
-            if t == 0:
+            if t == 0:  # input BOS, 304
                 it = fc_feats.data.new(batch_size).long().fill_(init_index)
             elif sample_max:
                 sampleLogprobs, it = torch.max(logprobs.data, 1)
@@ -238,16 +315,18 @@ class ShowAttendTellModel(nn.Module):
                     prob_prev = torch.exp(torch.div(logprobs.data, temperature)).cpu()
 
                 it = torch.multinomial(prob_prev, 1).cuda()
+
                 sampleLogprobs = logprobs.gather(1, Variable(it, requires_grad=False).cuda())  # gather the logprobs at sampled positions
                 it = it.view(-1).long()  # and flatten indices for downstream processing
 
             xt = self.embed(Variable(it, requires_grad=False).cuda())
 
             if t >= 1:
+                # stop when all finished
                 if t == 1:
                     unfinished = it > 0
                 else:
-                    unfinished *= (it > 0)
+                    unfinished = unfinished * (it > 0)
                 if unfinished.sum() == 0:
                     break
                 it = it * unfinished.type_as(it)
@@ -256,7 +335,7 @@ class ShowAttendTellModel(nn.Module):
 
             output, state = self.core.forward(xt, att_feats, state)
 
-            logprobs = F.log_softmax(self.logit(output), dim=1)
+            logprobs = F.log_softmax(self.logit(output))
             logprobs_all.append(logprobs)
 
         greedy_seq = torch.cat([_.unsqueeze(1) for _ in seq], 1)
@@ -275,7 +354,7 @@ class ShowAttendTellModel(nn.Module):
         outputs = []
 
         for i in range(seq.size(1)):
-            if i >= 1 and self.ss_prob > 0.0:  # otherwiste no need to sample
+            if i >= 1 and self.ss_prob > 0.0:  # otherwise no need to sample
                 sample_prob = fc_feats.data.new(batch_size).uniform_(0, 1)
                 sample_mask = sample_prob < self.ss_prob
                 if sample_mask.sum() == 0:
@@ -302,7 +381,7 @@ class ShowAttendTellModel(nn.Module):
                 output = F.log_softmax(self.logit(output.squeeze(0)), dim=1)
             outputs.append(output)
 
-        # return hidden states
+        # 返回 hidden states
         return state[0], outputs
 
     def free_running_get_hidden_states(self, fc_feats, att_feats, init_index, end_index):
